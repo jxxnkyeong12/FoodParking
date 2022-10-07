f1756a8478f17f15f20afb5db2e088c6ca3258e5
jxxnkyeong12 <znzlzn651@gmail.com>
2022-10-07 11:37:15 +0900

    진경진짜 다시2022/10/07

---
+++ Team_Project01/app/src/main/java/com/example/team_project01/store/AndStoreInfoFileVO.java
@@ -0,0 +1,29 @@
+package com.example.team_project01.store;
+
+public class AndStoreInfoFileVO {
+	private int store_code;
+	private String store_logo_name, store_logo_path;
+	
+	
+	public int getStore_code() {
+		return store_code;
+	}
+	public void setStore_code(int store_code) {
+		this.store_code = store_code;
+	}
+	public String getStore_logo_name() {
+		return store_logo_name;
+	}
+	public void setStore_logo_name(String store_logo_name) {
+		this.store_logo_name = store_logo_name;
+	}
+	public String getStore_logo_path() {
+		return store_logo_path;
+	}
+	public void setStore_logo_path(String store_logo_path) {
+		this.store_logo_path = store_logo_path;
+	}
+	
+	
+	
+}
